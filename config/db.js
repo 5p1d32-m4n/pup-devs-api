4ae22eeabdb1687bd0f926212e2f5153c05130ee
Brandon J. Baker <brandon.j.baker1011@gmail.com>
2023-05-31 11:05:59 -0400

    fixed minor grammatical error.

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 const connectToDB = (databaseURI) => {
-    console.log(`Connectino to ${databaseURI}`)
+    console.log(`Connecting to ${databaseURI}`)
     return mongoose.createConnection(databaseURI, {
         useNewUrlParser: true,
         useUnifiedTopology: true
