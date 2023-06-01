2ae5347a9576d6be11bbe86b6bf82dcc2bd05f1d
Brandon J. Baker <brandon.j.baker1011@gmail.com>
2023-06-01 14:44:43 -0400

    changed URL source from faker js for 'fashion'

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -90,7 +90,7 @@ const generateDummyProducts = async (req, res) => {
                 description: faker.commerce.productDescription(),
                 price: faker.commerce.price(),
                 category: faker.commerce.department(),
-                image: faker.image.abstract(width = 210, height = 235, randomize = true),
+                image: faker.image.fashion(width = 210, height = 235, randomize = true),
                 rating: Math.ceil(Math.random() * 5),
             })
             console.log(product)
